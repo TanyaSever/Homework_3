7e7fca91ecc4ab4b480081389b56eebbef8668c9
TanyaSever <tanya13sever@gmail.com>
2023-03-12 11:24:14 +1000

    TaskOneTaskTwo

---
+++ ThirdTask/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+void Method(int N)
+{
+    int i = 1;
+    while(N>=i)
+    {
+        Console.WriteLine(Math.Pow(i, 3));
+        i++;
+    }
+}
+int MyN = int.Parse(Console.ReadLine()!);
+Method(MyN);
